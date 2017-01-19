e52578e7e1d7cdbb3c5ac3da6252580db5acaf49
Jake Lazaroff <yo@jake.nyc>
2017-01-18 20:33:07 -0500

    Refactor a bit

--- test/task/test.js
+++ test/task/test.js
@@ -10,9 +10,5 @@ describe('Task', () => {
       const command = 'test';
       new Task(command).command.should.equal(command);
     });
-
-    it('should set its state to "queued"', () => {
-      new Task().state.should.equal('queued');
-    });
   });
 });
