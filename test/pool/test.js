b3266249fc4e0e555832b949e0dacc787c50e146
Jake Lazaroff <yo@jake.nyc>
2017-01-18 23:12:32 -0500

    Finish pool tests

--- test/pool/test.js
+++ test/pool/test.js
@@ -42,7 +42,7 @@ describe('Pool', () => {
     });
 
     it('should default to the number of workers returned by os.cpus()', () => {
-
+      os.cpus.should.be.calledOnce;
       Object.keys(new Pool(path).workers).length.should.equal(5);
     });
 
@@ -68,6 +68,98 @@ describe('Pool', () => {
     });
   });
 
+  describe('.run', () => {
+
+    it('should run the task immediately if there are idle workers', () => {
+      pool = new Pool(path, {
+        workers: 1
+      });
+
+      const task = pool.run('wait');
+      task.state.should.equal('running');
+    });
+
+    describe('queue', () => {
+
+      it('should queue the task if every worker is busy', () => {
+        pool = new Pool(path, {
+          workers: 1
+        });
+
+        const task1 = pool.run('wait'),
+              task2 = pool.run('test');
+
+        task2.state.should.equal('queued');
+        pool.tasks[0].should.equal(task2);
+      });
+
+      it('should run a queued task as soon as a worker becomes idle', done => {
+        pool = new Pool(path, {
+          workers: 1
+        });
+
+        const task1 = pool.run('wait'),
+              task2 = pool.run('wait').on('start', done);
+
+        task1._send('go');
+      });
+    });
+  });
+
+  describe('.on', () => {
+
+    beforeEach(() => {
+      pool = new Pool(path, {
+        workers: 1
+      });
+    });
+
+    describe('start', () => {
+
+      it('should proxy task "start" events', done => {
+        const task1 = pool.run('wait'),
+              task2 = pool.run('wait');
+
+        pool.on('start', _task => {
+          _task.should.equal(task2);
+          done();
+        });
+
+        task1._send('go');
+      });
+    });
+
+    describe('end', () => {
+
+      let task;
+
+      it('should proxy task "end" events', done => {
+        let task;
+
+        pool.on('end', (message, _task) => {
+          _task.should.equal(task);
+          done();
+        });
+
+        task = pool.run('');
+      });
+    });
+
+    describe('error', () => {
+
+      it('should proxy task "error" events', done => {
+        let task;
+
+        pool.on('error', (err, _task) => {
+          _task.should.equal(task);
+          done();
+        });
+
+        task = pool.run('error');
+      });
+    });
+  });
+
   describe('.available()', () => {
 
     it('should return the number of idle workers', done => {
@@ -77,16 +169,13 @@ describe('Pool', () => {
 
       pool.available().should.equal(1);
 
-      pool.on('start', () => {
-        pool.available().should.equal(0);
-      });
-
-      pool.on('end', () => {
+      const task = pool.run('wait').on('end', () => {
         pool.available().should.equal(1);
         done();
       });
 
-      pool.run('');
+      pool.available().should.equal(0);
+      task._send('go');
     });
   });
 
@@ -99,12 +188,10 @@ describe('Pool', () => {
 
       pool.total().should.equal(1);
 
-      pool.on('start', () => {
-        pool.total().should.equal(1);
-        done();
-      });
+      const task = pool.run('wait').on('end', done);
 
-      pool.run('');
+      pool.total().should.equal(1);
+      task._send('go');
     });
   });
 
--- test/pool/worker.js
+++ test/pool/worker.js
@@ -4,7 +4,34 @@ const Thread = require('../../lib/thread');
 
 class Worker extends Thread {
 
-  run (command) {
+  constructor () {
+    super();
+
+    this.wait = true;
+
+    process.on('message', data => {
+      if (data.type === 'go') {
+        this.wait = false;
+      }
+    });
+  }
+
+  run (command, callback) {
+
+    if (command === 'error') {
+      throw new Error();
+    }
+
+    this.wait = command === 'wait';
+    this.busyWait(callback);
+  }
+
+  busyWait (callback) {
+    if (this.wait) {
+      setTimeout(() => this.busyWait(callback), 0);
+    } else {
+      callback();
+    }
   }
 
 }
