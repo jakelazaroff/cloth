2cd480e2faccac82b35a96ca5cd3747f2a4ee44a
Jake Lazaroff <yo@jake.nyc>
2017-01-20 22:53:18 -0500

    Copy worker code between tests

---
+++ test/pool/worker.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Thread = require('../../lib/thread');
+
+class Worker extends Thread {
+
+  constructor () {
+    super();
+
+    this.wait = true;
+
+    process.on('message', data => {
+      if (data.type === 'go') {
+        this.wait = false;
+      }
+    });
+  }
+
+  run (command, callback) {
+
+    this.send('command', command);
+
+    if (command === 'error') {
+      throw new Error();
+    }
+
+    this.wait = command === 'wait';
+    this.busyWait(callback);
+  }
+
+  busyWait (callback) {
+    if (this.wait) {
+      setTimeout(() => this.busyWait(callback), 0);
+    } else {
+      this.send('test', 'test');
+      callback(null, 'return');
+    }
+  }
+
+}
+
+new Worker();
